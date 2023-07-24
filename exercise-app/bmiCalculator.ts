90a38b9304e8b22457eebfcfb096a8292431d467
Robert Grayson <robert.samuel.grayson@gmail.com>
2023-07-24 16:42:14 +0900

    Complete 9.5

--- exercise-app/bmiCalculator.ts
+++ exercise-app/bmiCalculator.ts
@@ -18,48 +18,48 @@ const parseArguments = (args: string[]): CalcValues => {
 }
 
 const calculateBmi = (height: number, weight: number): string => {
-  try {
-    const { height, weight } = parseArguments(process.argv);
-    const heightInMetres = height/100
-    const bmi = weight / (heightInMetres*heightInMetres)
-    let category
-    switch (true) {
-      case bmi < 16.0:
-        category = "Underweight (Severe thinness)";
-        break;
-      case bmi >= 16.0 && bmi <= 16.9:
-        category = "Underweight (Moderate thinness)";
-        break;
-      case bmi >= 17.0 && bmi <= 18.4:
-        category = "Underweight (Mild thinness)";
-        break;
-      case bmi >= 18.5 && bmi <= 24.9:
-        category = "Normal range";
-        break;
-      case bmi >= 25.0 && bmi <= 29.9:
-        category = "Overweight (Pre-obese)";
-        break;
-      case bmi >= 30.0 && bmi <= 34.9:
-        category = "Obese (Class I)";
-        break;
-      case bmi >= 35.0 && bmi <= 39.9:
-        category = "Obese (Class II)";
-        break;
-      default:
-        category = "Obese (Class III)";
-        break;
-    }
-    return category
-  } catch (error: unknown) {
-    let errorMessage = 'Something bad happened.'
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    return errorMessage;
+  const heightInMetres = height/100
+  const bmi = weight / (heightInMetres*heightInMetres)
+  let category
+  switch (true) {
+    case bmi < 16.0:
+      category = "Underweight (Severe thinness)";
+      break;
+    case bmi >= 16.0 && bmi <= 16.9:
+      category = "Underweight (Moderate thinness)";
+      break;
+    case bmi >= 17.0 && bmi <= 18.4:
+      category = "Underweight (Mild thinness)";
+      break;
+    case bmi >= 18.5 && bmi <= 24.9:
+      category = "Normal range";
+      break;
+    case bmi >= 25.0 && bmi <= 29.9:
+      category = "Overweight (Pre-obese)";
+      break;
+    case bmi >= 30.0 && bmi <= 34.9:
+      category = "Obese (Class I)";
+      break;
+    case bmi >= 35.0 && bmi <= 39.9:
+      category = "Obese (Class II)";
+      break;
+    default:
+      category = "Obese (Class III)";
+      break;
   }
+  return category
 }
 
-const height: number = Number(process.argv[2])
-const weight: number = Number(process.argv[3])
 
-console.log(calculateBmi(height, weight))
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
+export default calculateBmi
--- exercise-app/index.ts
+++ exercise-app/index.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (_req, res) => {
+    const height: number = Number(_req.query.height);
+    const weight: number = Number(_req.query.weight);
+
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        res.status(400).send('Malformatted parameters');
+      } else {
+        const bmi: string = calculateBmi(height, weight);
+        res.send({
+            'height': height,
+            'weight': weight,
+            'bmi': bmi
+        });
+      }
+  });
 
 const PORT = 3003;
 
