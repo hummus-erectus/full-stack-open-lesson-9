2872438ec30945426941699ffdeb3fda1aa84306
Robert Grayson <robert.samuel.grayson@gmail.com>
2023-07-25 16:25:04 +0900

    Complete tutorial part The Horrors of Any

--- typescript-tutorial/index.ts
+++ typescript-tutorial/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { calculator } from './calculator';
+
 const app = express();
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
@@ -9,4 +12,17 @@ const PORT = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if ( !value1 || isNaN(Number(value1)) ) {
+    return res.status(400).send({ error: '...'});
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculator(value1, value2, op);
+  res.send({ result });
 });
